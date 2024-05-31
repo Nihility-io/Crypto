81b849861ed7bce4d643c41c1f6633e82135b108
Nihility-io <Nihility-io>
2024-06-01 01:07:44 +0200

    Initial version

---
+++ crypto_test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "@std/assert"
+import { decrypt, encrypt, EncryptionAlgorithm, randomBytes } from "./mod.ts"
+
+Deno.test("Encryption/Decryption", async (t) => {
+	const key = randomBytes(32)
+	const pass = "abc123"
+	const message = "Hello World!"
+
+	await t.step("Password", async () => {
+		const enc = await encrypt(key, pass, message, EncryptionAlgorithm.AES256GCM)
+		const dec = await decrypt(key, pass, enc)
+		assertEquals(dec, message)
+	})
+
+	await t.step("No Password", async () => {
+		const enc = await encrypt(key, "", message, EncryptionAlgorithm.AES256GCM)
+		const dec = await decrypt(key, "", enc)
+		assertEquals(dec, message)
+	})
+})
